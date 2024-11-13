5c0564156bb5041c469be4639e7e6361bf8ed11b
rafaelllferreira <rafaelferreira22012@gmail.com>
2024-11-13 18:38:25 -0300

    Add files via upload

---
+++ aula_01_01.py
@@ -0,0 +1,5 @@
+n1 = 10
+n2 = 5.6
+nome = "Rafael"
+Idade = 26
+print("Sr(a)", nome, ", a sua idade é", Idade) 
---
+++ aula_01_02.py
@@ -0,0 +1,3 @@
+num = int(input("informe um valor: "))
+result = num * 2
+print("O dobro do número é ", result)
